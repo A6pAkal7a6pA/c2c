4f047b8abb8412608fca8fd110e0e31b0f12f789
Maksym Kuleshov <maksym_kuleshov@epam.com>
2022-11-03 10:42:45 +0200

    added js

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let header = document.querySelector('.header');
+let headerToggle = document.querySelector('.header__toggle');
+
+headerToggle.addEventListener('click', () => {
+	console.log('click');
+	header.classList.toggle('header_active');
+});
+
+let filterToggle = document.querySelectorAll('.filter-group__head');
+
+filterToggle.forEach(filter => {
+	filter.addEventListener('click', () => {
+		filter.parentElement.classList.toggle('filter-group__category_active');
+	})
+});
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+for (let anchor of anchors) {
+	anchor.addEventListener('click', function (e) {
+		e.preventDefault()
+		const blockID = anchor.getAttribute('href').substr(1)
+		console.log(blockID);
+		header.classList.remove('header_active');
+		document.getElementById(blockID).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start',
+			inline: 'start'
+		})
+	})
+}
